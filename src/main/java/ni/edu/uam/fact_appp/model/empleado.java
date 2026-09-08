13362dd331f13f78c3ede26da743d8c033318c1f
Aris Gavarrete <amgavarrete@uamv.edu.ni>
2026-09-08 16:21:28 -0600

    Update Cargo, Empleado

--- src/main/java/ni/edu/uam/fact_appp/model/empleado.java
+++ src/main/java/ni/edu/uam/fact_appp/model/empleado.java
@@ -1,4 +1,16 @@
 package ni.edu.uam.fact_appp.model;
 
+import lombok.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class empleado {
+    private Integer id;
+    private String nombres;
+    private String apellidos;
+    private Cargo cargo;
+    private LocalDate fechaContratacion;
+    private boolean activo;
 }
