f5e30104b0d391b148ad44e7c2110030c6a5fe5d
Aris Gavarrete <amgavarrete@uamv.edu.ni>
2026-09-08 16:01:26 -0600

    Agregar producto

--- src/main/java/ni/edu/uam/fact_appp/model/Categoria.java
+++ src/main/java/ni/edu/uam/fact_appp/model/Categoria.java
@@ -1,4 +1,24 @@
 package ni.edu.uam.fact_appp.model;
 
+import lombok.*;
+
 public class Categoria {
+
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CategoriaDTO {
+        private Integer id;
+        private String nombre;
+        private boolean activa;
+
+
+
+        @Override
+        public String  toString() {
+            return nombre;
+        }
+
+    }
 }
--- src/main/java/ni/edu/uam/fact_appp/model/Producto.java
+++ src/main/java/ni/edu/uam/fact_appp/model/Producto.java
@@ -1,4 +1,23 @@
 package ni.edu.uam.fact_appp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Producto {
+
+
+    private Integer id;
+    private String nombre;
+    private BigDecimal precioVenta;
+    private Categoria categoria;
+    private int exitencia;
+    private String rutaImagen;
+    private boolean activo;
+
 }
