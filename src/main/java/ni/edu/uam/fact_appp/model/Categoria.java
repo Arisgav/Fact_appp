8772cd24bec9763d1e7b9014782e7991ec160ae4
Aris Gavarrete <amgavarrete@uamv.edu.ni>
2026-09-10 17:24:30 -0600

    Correccion de categoria

--- src/main/java/ni/edu/uam/fact_appp/model/Categoria.java
+++ src/main/java/ni/edu/uam/fact_appp/model/Categoria.java
@@ -1,27 +1,20 @@
 package ni.edu.uam.fact_appp.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Categoria {
 
+    private Integer id;
+    private String nombre;
+    private boolean activa;
 
-    public Categoria(int i, String limpieza, boolean b) {
-    }
-
-    @Data
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public static class CategoriaDTO {
-        private Integer id;
-        private String nombre;
-        private boolean activa;
-
-
-
-        @Override
-        public String  toString() {
-            return nombre;
-        }
-
+    @Override
+    public String toString() {
+        return nombre;
     }
 }
